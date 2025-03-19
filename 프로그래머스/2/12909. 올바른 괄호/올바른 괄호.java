64d4e1bfb73d90e26dc036d1b41e202ad5836fbc
BackJiwan <95860566+BackJiwan@users.noreply.github.com>
2025-03-19 21:30:48 +0900

    [level 2] Title: 올바른 괄호, Time: 16.94 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    boolean solution(String s) {
+
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+    
+        char[] array = s.toCharArray();
+        
+        for(char c: array){
+            if(c=='('){
+                stack.push(c);
+            }else{ //닫는괄호가 나온경우
+                if(stack.isEmpty() || stack.pop() == c){
+                    return false;
+                }
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+		return false;
+    }
+}
