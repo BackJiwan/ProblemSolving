ccfbf8f3c7b3a4ae587674d9a3f368d0d5c1e474
BackJiwan <95860566+BackJiwan@users.noreply.github.com>
2025-04-11 13:00:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 11.22 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,25 +1,22 @@
-/*
-1. 100개 있으니까 그냥 전부다 i랑 j로 순회하면서 ^2 으로 봐도됨
-2. 다만 중복을 허용하지 않도록 Set에 담아서 관리하면 될듯
-*/
 import java.util.HashSet;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        int n = numbers.length;
         
+        //만들수 있는 모든수인 answer는 set으로 중복을 자동 제거 한다. 
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<n-1;i++){//왼쪽
-            for(int j=i+1;j<n;j++){//오른쪽 
-                int sum = numbers[i] + numbers[j];
-                set.add(sum);
+        
+        for(int i=0;i<numbers.length-1;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                set.add(numbers[i] + numbers[j]);
             }
         }
         
-        //set을 오름차순으로 정렬하고 다시 int[] 로 만들어야 한다. 
+        //return set.stream().sorted().mapToInt(Integer::intValue).toArray();
         
-        // HashSet<Integer> -> 스트림을 만들까...?
-        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        //return set.stream().sorted((o1,o2)->Integer.compare(o1,o2)).mapToInt(Integer::intValue).toArray();
+        
+        return set.stream().sorted((o1,o2) -> o1.compareTo(o2)).mapToInt(Integer::intValue).toArray();
 
     }
 }
