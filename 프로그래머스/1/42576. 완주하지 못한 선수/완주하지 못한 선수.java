63b757c8e5ab0cd64797472579de305504c5dd45
BackJiwan <95860566+BackJiwan@users.noreply.github.com>
2025-04-16 12:51:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.05 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,24 +1,22 @@
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        //map 선수,완주횟수 
-        HashMap<String,Integer> map = new HashMap<>();   
-        //완주한 선수들을 모두 map에 등록
-        for(String s:completion){
-            map.put(s,map.getOrDefault(s,0)+1);
-        }
+        String answer = "";
+        // return answer;
         
-        for(String s : participant){
-            if(map.getOrDefault(s,0)==0){
-                //참가선수들을 하나씩 빼서 value를 -1 씩하다가 value가 0을 만나면 
-                //완주하지 않았고, 참가만한 선수를 detect
-                //동명이인 2명중 1명만 완주한 경우 2번째로 참가선수를 꺼냈을 경우에 detect
-                return s;
+        HashSet<String> set = new HashSet<>(Arrays.asList(participant));
+        
+        for(String s : completion){
+            if(set.contains(s)){
+                continue;
             }else{
-                map.put(s,map.getOrDefault(s,0)-1);
+                answer = s;
             }
         }
-        return null;
+        
+        //System.out.println(set.toString());
+        return answer;
     }
 }
